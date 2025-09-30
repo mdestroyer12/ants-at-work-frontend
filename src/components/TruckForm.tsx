0fced7a8214f3d21402faec563c81fe78ca44725
Pedro Anuda <pedroanuda@gmail.com>
2025-09-30 01:10:28 -0300

    wip: refactor: change dialog use in TruckForm

--- src/components/TruckForm.tsx
+++ src/components/TruckForm.tsx
@@ -1,20 +1,30 @@
 import { useForm } from "react-hook-form";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import api from "../api/axios";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { truckSchema, TruckData } from "../schemas/TruckSchema";
+import { truckSchema, TruckData } from "@schemas/TruckSchema";
 import Input from "./Input";
 import { Button } from "./Button";
-import Loader from "./Loader";
+import { useQuery } from "@tanstack/react-query";
+import { Dialog, DialogHeader, DialogContent, DialogTitle, DialogFooter } from "@components/shadcn-ui/Dialog";
 
 type Props = {
+  open: boolean;
   onSubmit: (data: TruckData) => void;
-  onClose: () => void;
-  loading?: boolean;
+  handleOpenChange: (open: boolean) => void;
 };
 
-export default function TruckForm({ onSubmit, onClose, loading }: Props) {
-  const [truckTypes, setTruckTypes] = useState<string[]>([]);
+async function fetchTruckTypes() {
+  try {
+    const res = await api.get("/truck-types");
+    return res.data;
+  } catch (err) {
+    console.error("Falha ao buscar tipos de caminhão:", err);
+    return ["Baú", "Sider", "Graneleiro", "Refrigerado"];
+  }
+}
+
+export default function TruckForm({ open, onSubmit, handleOpenChange }: Props) {
   const [step, setStep] = useState(1);
 
   const {
@@ -27,18 +37,11 @@ export default function TruckForm({ onSubmit, onClose, loading }: Props) {
     mode: "onBlur",
   });
 
-  useEffect(() => {
-    async function fetchTruckTypes() {
-      try {
-        const res = await api.get("/truck-types");
-        setTruckTypes(res.data);
-      } catch (err) {
-        console.error("Falha ao buscar tipos de caminhão:", err);
-        setTruckTypes(["Baú", "Sider", "Graneleiro", "Refrigerado"]);
-      }
-    }
-    fetchTruckTypes();
-  }, []);
+  const { data: truckTypes } = useQuery<string[]>({
+    initialData: ["Baú", "Sider", "Graneleiro", "Refrigerado"],
+    queryKey: ["truckTypes"],
+    queryFn: fetchTruckTypes,
+  })
 
   const handleNextStep = async () => {
     const fieldsToValidate: (keyof TruckData)[] = [
@@ -61,179 +64,174 @@ export default function TruckForm({ onSubmit, onClose, loading }: Props) {
   };
 
   return (
-    <div className="relative">
-      <button
-        onClick={onClose}
-        className="absolute top-4 right-4 text-gray-500 hover:text-gray-800 transition-colors z-10"
-        aria-label="Fechar modal"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-        </svg>
-      </button>
-
+    <Dialog open={open} onOpenChange={handleOpenChange} >
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3 p-4">
-        <div className="text-center mb-4 pt-6"> 
-          <p className="text-lg font-bold text-[#4C2D2D]">
-            Etapa {step} de 2:{" "}
-            {step === 1 ? "Dados Básicos" : "Dados de Manutenção"}
-          </p>
-          <div className="w-full bg-gray-200 rounded-full h-2.5 mt-2">
-            <div
-              className="bg-[#744625] h-2.5 rounded-full transition-all duration-500"
-              style={{ width: step === 1 ? "50%" : "100%" }}
-            ></div>
-          </div>
-        </div>
-
-        {step === 1 && (
-          <>
-            <Input
-              text="Placa"
-              id="plate"
-              type="text"
-              placeholder="Insira a placa..."
-              register={register}
-              error={errors.plate?.message}
-            />
-            <Input
-              text="Modelo"
-              id="model"
-              type="text"
-              placeholder="Insira o modelo..."
-              register={register}
-              error={errors.model?.message}
-            />
-            <Input
-              text="Capacidade Máxima (kg)"
-              id="capacity"
-              type="number"
-              placeholder="Insira a capacidade em kg..."
-              register={register}
-              error={errors.capacity?.message}
-            />
-            <p className="text-[#4C2D2D] font-medium -mb-1">
-              Dimensões Internas
-            </p>
-            <div className="flex gap-2">
-              <Input
-                text="Comprimento (m)"
-                id="length"
-                type="number"
-                placeholder="Ex: 14.5"
-                register={register}
-                error={errors.length?.message}
-              />
-              <Input
-                text="Largura (m)"
-                id="width"
-                type="number"
-                placeholder="Ex: 2.6"
-                register={register}
-                error={errors.width?.message}
-              />
-              <Input
-                text="Altura (m)"
-                id="height"
-                type="number"
-                placeholder="Ex: 3.1"
-                register={register}
-                error={errors.height?.message}
-              />
+        <DialogContent className="max-w-10xl max-h-[90vh] overflow-y-auto">
+          <DialogHeader>
+            <DialogTitle className="text-center text-lg font-bold text-[#4C2D2D]">
+              Etapa {step} de 2:{" "}
+              {step === 1 ? "Dados Básicos" : "Dados de Manutenção"}
+            </DialogTitle>
+            <div className="text-center mb-4"> 
+              <div className="w-full bg-gray-200 rounded-full h-2.5 mt-2">
+                <div
+                  className="bg-[#744625] h-2.5 rounded-full transition-all duration-500"
+                  style={{ width: step === 1 ? "50%" : "100%" }}
+                ></div>
+              </div>
             </div>
-            <div className="flex flex-col w-full mt-2">
-              <label
-                htmlFor="type"
-                className="mb-1 text-[#4C2D2D] font-medium"
-              >
-                Tipo
-              </label>
-              <select
-                id="type"
-                {...register("type")}
-                className="w-full p-2 rounded-lg border-2 border-[#CABAAE] bg-[#E5DAD1] text-[#3F2323] text-sm hover:border-[#4C2D2D] focus:outline-none focus:ring-2 focus:ring-[#744625] transition"
-              >
-                <option value="">Selecione o tipo</option>
-                {truckTypes.map((t) => (
-                  <option key={t} value={t}>
-                    {t}
-                  </option>
-                ))}
-              </select>
-              {errors.type && (
-                <p className="text-[#800000] text-xs mt-1 ml-1 font-medium">
-                  {errors.type.message}
-                </p>
+          </DialogHeader>
+
+          {step === 1 && (
+            <>
+              <Input
+                text="Placa"
+                id="plate"
+                type="text"
+                placeholder="Insira a placa..."
+                register={register}
+                error={errors.plate?.message}
+              />
+              <Input
+                text="Modelo"
+                id="model"
+                type="text"
+                placeholder="Insira o modelo..."
+                register={register}
+                error={errors.model?.message}
+              />
+              <Input
+                text="Capacidade Máxima (kg)"
+                id="capacity"
+                type="number"
+                placeholder="Insira a capacidade em kg..."
+                register={register}
+                error={errors.capacity?.message}
+              />
+              <p className="text-[#4C2D2D] font-medium -mb-1">
+                Dimensões Internas
+              </p>
+              <div className="flex gap-2">
+                <Input
+                  text="Comprimento (m)"
+                  id="length"
+                  type="number"
+                  placeholder="Ex: 14.5"
+                  register={register}
+                  error={errors.length?.message}
+                />
+                <Input
+                  text="Largura (m)"
+                  id="width"
+                  type="number"
+                  placeholder="Ex: 2.6"
+                  register={register}
+                  error={errors.width?.message}
+                />
+                <Input
+                  text="Altura (m)"
+                  id="height"
+                  type="number"
+                  placeholder="Ex: 3.1"
+                  register={register}
+                  error={errors.height?.message}
+                />
+              </div>
+              <div className="flex flex-col w-full mt-2">
+                <label
+                  htmlFor="type"
+                  className="mb-1 text-[#4C2D2D] font-medium"
+                >
+                  Tipo
+                </label>
+                <select
+                  id="type"
+                  {...register("type")}
+                  className="w-full p-2 rounded-lg border-2 border-[#CABAAE] bg-[#E5DAD1] text-[#3F2323] text-sm hover:border-[#4C2D2D] focus:outline-none focus:ring-2 focus:ring-[#744625] transition"
+                >
+                  <option value="">Selecione o tipo</option>
+                  {truckTypes.map((t) => (
+                    <option key={t} value={t}>
+                      {t}
+                    </option>
+                  ))}
+                </select>
+                {errors.type && (
+                  <p className="text-[#800000] text-xs mt-1 ml-1 font-medium">
+                    {errors.type.message}
+                  </p>
+                )}
+              </div>
+              <div className="flex flex-col w-full mt-2">
+                <label
+                  htmlFor="detail"
+                  className="mb-1 text-[#4C2D2D] font-medium"
+                >
+                  Detalhes/Sobre
+                </label>
+                <textarea
+                  id="detail"
+                  placeholder="Observações adicionais sobre o caminhão..."
+                  {...register("detail")}
+                  className="w-full p-2 rounded-lg border-2 border-[#CABAAE] bg-[#E5DAD1] text-[#3F2323] text-sm hover:border-[#4C2D2D] focus:outline-none focus:ring-2 focus:ring-[#744625] transition"
+                  rows={3}
+                />
+                {errors.detail && (
+                  <p className="text-[#800000] text-xs mt-1 ml-1 font-medium">
+                    {errors.detail.message}
+                  </p>
+                )}
+              </div>
+            </>
+          )}
+
+          {step === 2 && (
+            <>
+              <Input
+                text="Última Revisão"
+                id="lastRevision"
+                type="date"
+                placeholder=""
+                register={register}
+                error={errors.lastRevision?.message}
+              />
+              <Input
+                text="Quilometragem"
+                id="mileage"
+                type="number"
+                placeholder="Insira a quilometragem atual"
+                register={register}
+                error={errors.mileage?.message}
+              />
+            </>
+          )}
+
+          <DialogFooter className="w-full sm:justify-between">
+            <div>
+              {step === 2 && (
+                <Button type="button" onClick={handlePrevStep}>
+                  Voltar
+                </Button>
               )}
             </div>
-            <div className="flex flex-col w-full mt-2">
-              <label
-                htmlFor="detail"
-                className="mb-1 text-[#4C2D2D] font-medium"
-              >
-                Detalhes/Sobre
-              </label>
-              <textarea
-                id="detail"
-                placeholder="Observações adicionais sobre o caminhão..."
-                {...register("detail")}
-                className="w-full p-2 rounded-lg border-2 border-[#CABAAE] bg-[#E5DAD1] text-[#3F2323] text-sm hover:border-[#4C2D2D] focus:outline-none focus:ring-2 focus:ring-[#744625] transition"
-                rows={3}
-              />
-              {errors.detail && (
-                <p className="text-[#800000] text-xs mt-1 ml-1 font-medium">
-                  {errors.detail.message}
-                </p>
+
+            <div>
+              {step === 1 && (
+                <Button type="button" onClick={handleNextStep}>
+                  Próximo
+                </Button>
+              )}
+
+              {step === 2 && (
+                <Button type="submit">
+                  {/* {loading ? <Loader /> : "Salvar"} */}
+                  Salvar
+                </Button>
               )}
             </div>
-          </>
-        )}
-
-        {step === 2 && (
-          <>
-            <Input
-              text="Última Revisão"
-              id="lastRevision"
-              type="date"
-              placeholder=""
-              register={register}
-              error={errors.lastRevision?.message}
-            />
-            <Input
-              text="Quilometragem"
-              id="mileage"
-              type="number"
-              placeholder="Insira a quilometragem atual"
-              register={register}
-              error={errors.mileage?.message}
-            />
-          </>
-        )}
-
-        <div className="flex justify-between mt-4">
-          <div>
-            {step === 2 && (
-              <Button type="button" onClick={handlePrevStep}>
-                Voltar
-              </Button>
-            )}
-          </div>
-
-          <div>
-            {step === 1 && (
-              <Button type="button" onClick={handleNextStep}>
-                Próximo
-              </Button>
-            )}
-
-            {step === 2 && (
-              <Button type="submit" disabled={loading}>
-                {loading ? <Loader /> : "Salvar"}
-              </Button>
-            )}
-          </div>
-        </div>
+          </DialogFooter>
+        </DialogContent>
       </form>
-    </div>
+    </Dialog>
   );
 }
--- src/pages/Trucks/TruckList.tsx
+++ src/pages/Trucks/TruckList.tsx
@@ -9,8 +9,11 @@ import { Skeleton } from "@components/shadcn-ui/Skeleton";
 import { ColumnDef } from "@tanstack/react-table";
 import { ViewDataTable } from "@components/trucks/ViewDataTable";
 import { Pencil, Trash } from "lucide-react";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@components/shadcn-ui/Tooltip";
-import { Dialog, DialogContent } from "@components/Dialog";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@components/shadcn-ui/Tooltip";
 
 const tableColumns: ColumnDef<Truck>[] = [
   {
@@ -41,56 +44,69 @@ const tableColumns: ColumnDef<Truck>[] = [
         <div className="flex gap-2 justify-end">
           <Tooltip>
             <TooltipTrigger>
-              <Button variant="outline">
+              <Button variant="outline" className="cursor-pointer" onClick={() => console.log("Editando " + truck.id)}>
                 <Pencil />
               </Button>
             </TooltipTrigger>
-            <TooltipContent>
-              Editar caminhão
-            </TooltipContent>
+            <TooltipContent>Editar caminhão</TooltipContent>
           </Tooltip>
           <Tooltip>
             <TooltipTrigger>
-              <Button variant="destructive">
+              <Button variant="destructive" className="cursor-pointer">
                 <Trash />
               </Button>
             </TooltipTrigger>
-            <TooltipContent>
-              Excluir caminhão
-            </TooltipContent>
+            <TooltipContent>Excluir caminhão</TooltipContent>
           </Tooltip>
-          
         </div>
       );
-    }
-  }
-]
+    },
+  },
+];
 
 async function fetchTrucks(): Promise<Truck[]> {
-  await new Promise(resolve => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const trucks = [
-    { id: "1", plate: "ABC-1234", model: "Ford F-150", capacity: 1000, mileage: 50000, lastRevision: "2023-10-01" },
-    { id: "2", plate: "DEF-5678", model: "Chevrolet Silverado", capacity: 1200, mileage: 60000, lastRevision: "2023-09-15" }
-  ]
+    {
+      id: "1",
+      plate: "ABC-1234",
+      model: "Ford F-150",
+      capacity: 1000,
+      mileage: 50000,
+      lastRevision: "2023-10-01",
+    },
+    {
+      id: "2",
+      plate: "DEF-5678",
+      model: "Chevrolet Silverado",
+      capacity: 1200,
+      mileage: 60000,
+      lastRevision: "2023-09-15",
+    },
+  ];
 
   return trucks;
 }
 
 async function handleAddTruck(data: TruckData) {
-    //api.post("/trucks", data);
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-  }
+  //api.post("/trucks", data);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+}
 
 export default function TruckList() {
   const [showForm, setShowForm] = useState(false);
 
-  const { data: trucks, isPending: isLoading, isError } = useQuery<Truck[]>({
-    queryKey: ['trucks'],
-    queryFn: fetchTrucks
-  })
+  const {
+    data: trucks,
+    isPending: isLoading,
+    isError,
+  } = useQuery<Truck[]>({
+    queryKey: ["trucks"],
+    queryFn: fetchTrucks,
+  });
 
   const truckMutation = useMutation({
-    mutationKey: ['save-trucks'],
+    mutationKey: ["save-trucks"],
     mutationFn: handleAddTruck,
     onSuccess: () => {
       toast.success("Caminhão adicionado com sucesso!");
@@ -98,8 +114,8 @@ export default function TruckList() {
     },
     onError: () => {
       toast.error("Erro ao adicionar caminhão. Tente novamente.");
-    }
-  })
+    },
+  });
 
   useEffect(() => {
     if (isError) {
@@ -116,51 +132,13 @@ export default function TruckList() {
       {isLoading ? (
         <Skeleton className="w-full h-[60vh] mb-2" />
       ) : (
-        // <table className="w-full border">
-        //   <thead>
-        //     <tr className="bg-[#E5DAD1] text-[#4C2D2D]">
-        //       <th className="p-3 text-left">Placa</th>
-        //       <th className="p-3 text-left">Modelo</th>
-        //       <th className="p-3 text-left">Capacidade (kg)</th>
-        //       <th className="p-3 text-left">Quilometragem</th>
-        //       <th className="p-3 text-left">Última Revisão</th>
-        //       <th className="p-3 text-left">Ações</th>
-        //     </tr>
-        //   </thead>
-        //   <tbody>
-        //     {trucks?.map((truck) => (
-        //       <tr key={truck.plate} className="border-b border-[#CABAAE] hover:bg-[#F0EBE5]">
-        //         <td className="p-3">{truck.plate}</td>
-        //         <td className="p-3">{truck.model}</td>
-        //         <td className="p-3">{truck.capacity}</td>
-        //         <td className="p-3">{truck.mileage}</td>
-        //         <td className="p-3">{new Date(truck.lastRevision).toLocaleDateString()}</td>
-        //         {/* Buttons somente visuais, sem funcionalidade real. Aguarde poder testar o front com o back para adicionar essas functions*/}
-        //         <td className="p-3">
-        //           <button className="text-blue-600 hover:underline">Editar</button>
-        //           <button className="text-red-600 hover:underline ml-4">Excluir</button>
-        //         </td>
-        //       </tr>
-        //     ))}
-        //   </tbody>
-        // </table>
         <ViewDataTable columns={tableColumns} data={trucks || []} />
       )}
-      <Dialog open={showForm}>
-        <DialogContent>
-          <TruckForm onSubmit={truckMutation.mutate} onClose={() => setShowForm(false)} loading={truckMutation.isPending} />
-        </DialogContent>
-      </Dialog>
-      {/* {showForm && (
-        <div 
-          className="fixed inset-0 flex items-center justify-center z-50"
-          style={{ backgroundColor: 'rgba(0, 0, 0, 0.74)' }}
-        >
-          <div className="bg-[#F9F5F2] rounded-lg shadow-xl p-2 max-w-lg w-full">
-            
-          </div>
-        </div>
-      )} */}
+      <TruckForm
+        onSubmit={truckMutation.mutate}
+        open={showForm}
+        handleOpenChange={(open) => setShowForm(open)}
+      />
     </div>
   );
-} 
+}
